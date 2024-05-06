43c394ac9eb503fa19f2e25acfa58139db4f4151
musarratChowdhury <muhitlopez143@gmail.com>
2024-05-06 11:44:41 +0600

    files added

---
+++ SQLbackup/ExpenseCategories.sql
@@ -0,0 +1,4 @@
+INSERT INTO fmdb.ExpenseCategories (Id, Name, EntryDate, CreatedBy) VALUES (1, 'shopping', '2024-05-06 04:56:44.340000', '00000000-0000-0000-0000-000000000000');
+INSERT INTO fmdb.ExpenseCategories (Id, Name, EntryDate, CreatedBy) VALUES (2, 'Education', '2024-05-06 05:06:25.755000', '00000000-0000-0000-0000-000000000000');
+INSERT INTO fmdb.ExpenseCategories (Id, Name, EntryDate, CreatedBy) VALUES (3, 'Medical', '2024-05-06 05:06:25.755000', '00000000-0000-0000-0000-000000000000');
+INSERT INTO fmdb.ExpenseCategories (Id, Name, EntryDate, CreatedBy) VALUES (4, 'Entertainment', '2024-05-06 05:06:25.755000', '00000000-0000-0000-0000-000000000000');
---
+++ SQLbackup/Expenses.sql
@@ -0,0 +1,4 @@
+INSERT INTO fmdb.Expenses (Id, Cause, Amount, EntryDate, CreatedBy, ExpenseCategoryId) VALUES (1, 'buy iron for ', 1000, '2024-05-06 05:02:44.259000', '00000000-0000-0000-0000-000000000000', 1);
+INSERT INTO fmdb.Expenses (Id, Cause, Amount, EntryDate, CreatedBy, ExpenseCategoryId) VALUES (2, 'buy 3 piece for wife', 700, '2024-05-06 05:02:44.259000', '00000000-0000-0000-0000-000000000000', 1);
+INSERT INTO fmdb.Expenses (Id, Cause, Amount, EntryDate, CreatedBy, ExpenseCategoryId) VALUES (3, 'Bought medicine', 1000, '2024-05-06 05:06:17.620000', '00000000-0000-0000-0000-000000000000', 3);
+INSERT INTO fmdb.Expenses (Id, Cause, Amount, EntryDate, CreatedBy, ExpenseCategoryId) VALUES (4, 'Bought Game Console', 1000, '2024-05-06 05:06:17.620000', '00000000-0000-0000-0000-000000000000', 4);
